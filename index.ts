9fb94b47ada0c881248e90473b7190ca4eb083ac
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2026-06-15 10:47:21 -0400

    Added .gitignore file

--- index.ts
+++ index.ts
@@ -2,45 +2,45 @@
 // basic type annotations
 //------------------------
 
-let age: number = 30
-let firstName: string = 'Mario'
-let isFictional: boolean
+let age: number = 30;
+let firstName: string = "Mario";
+let isFictional: boolean;
 
 // age = 'hello'
-age = 31
+age = 31;
 
 // firstName = 100
-firstName = 'Luigi'
+firstName = "Luigi";
 
 // isFictional = 'false'
-isFictional = false
+isFictional = false;
 
 //----------------
 // type inference
 //----------------
 
-let planet = 'Earth'
-let moons = 1
-let isLarge = false
+let planet = "Earth";
+let moons = 1;
+let isLarge = false;
 
 // planet = 10
-planet = 'Saturn'
+planet = "Saturn";
 
 // moons = '145'
-moons = 145
+moons = 145;
 
 // isLarge = 'yes'
-isLarge = true
+isLarge = true;
 
 //------------------
 // null & undefined
 //------------------
 
-let something: null
-let anotherThing: undefined
+let something: null;
+let anotherThing: undefined;
 
 // something = 10
-something = null
+something = null;
 
 // anotherThing = 'hello, world'
-anotherThing = undefined
+anotherThing = undefined;
